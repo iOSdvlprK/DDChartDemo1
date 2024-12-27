950a602c6b2a25d29b51f7e8eeafa341a5e18aff
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-12-27 23:25:10 +0900

    bar chart w/o chart framework

--- ChartDemo1/ChartDemo1App.swift
+++ ChartDemo1/ChartDemo1App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ChartDemo1App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TestColorButtonView()
         }
     }
 }
--- ChartDemo1/ContentView.swift
+++ ChartDemo1/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors = Color.defaultColors
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
