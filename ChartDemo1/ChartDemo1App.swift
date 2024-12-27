aced6bfb01aa3053cfe9e5979676cd3680a8df07
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-12-27 22:45:19 +0900

    Initial Commit

---
+++ ChartDemo1/ChartDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  ChartDemo1App.swift
+//  ChartDemo1
+//
+//  Created by joe on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ChartDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
